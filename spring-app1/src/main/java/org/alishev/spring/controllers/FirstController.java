041408add9514ec3892931b97556f8156efd346f
Alexander Karpenko <alexander_karpenko@epam.com>
2021-08-30 13:29:44 +0300

    first web mvc app with 2 controllers

---
+++ spring-app1/src/main/java/org/alishev/spring/controllers/FirstController.java
@@ -0,0 +1,21 @@
+package org.alishev.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage() {
+        return "first/hello";
+    }
+
+    @GetMapping("/bye")
+    public String byePage() {
+        return "first/bye";
+    }
+
+}
